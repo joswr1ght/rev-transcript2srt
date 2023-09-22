1e9ea26e78465ff622c8415b48a416ac38c509be
Joshua Wright <jwright@hasborg.com>
2023-09-22 06:21:27 -0400

    Improve quality of SRT timing by tracking the last caption end time, preventing overlapping timing markers

--- rev-transcript.py
+++ rev-transcript.py
@@ -45,42 +45,59 @@ if __name__ == '__main__':
         sys.stderr.write(f'Usage: {os.path.basename(sys.argv[0])} [Frost JSON file]\n')
         sys.exit(0)
 
-with open(sys.argv[1]) as f:
-    frost = json.loads(f.read())
-    frostappliedpmdoc = json.loads(frost['frostTranscript']['appliedPmDocJson'])
-    fcontent = frostappliedpmdoc['content'][0]['content']
+    with open(sys.argv[1]) as f:
+        frost = json.loads(f.read())
+        frostappliedpmdoc = json.loads(frost['frostTranscript']['appliedPmDocJson'])
+        fcontent = frostappliedpmdoc['content'][0]['content']
 
-    linecount = 1
-    for para in fcontent:
-        ptranscript = para['content'][0]['text']
-        ptimestamps = para['attrs']['timestamps']
-        captionindex = 0  # Index for word and timing marker lists
+        # Track the last time end marker for each sentence to ensure
+        # the next marker start does not happen happen before the last
+        # marker stop.
+        lastendtime = timedelta(seconds=0)
 
-        # Split the paragraph transcript content into sentences.
-        psentences = splitparagraph(ptranscript)
-        wordlist = ' '.join(psentences).split(' ')
+        linecount = 1
+        for para in fcontent:
+            ptranscript = para['content'][0]['text']
+            ptimestamps = para['attrs']['timestamps']
+            captionindex = 0  # Index for word and timing marker lists
 
-        # Each sentence becomes a SRT transcript line with timing markers
-        for psentence in psentences:
+            # Split the paragraph transcript content into sentences.
+            psentences = splitparagraph(ptranscript)
+            wordlist = ' '.join(psentences).split(' ')
 
-            # Some sentences are too long; split up longer sentences into
-            # smaller chunks.
-            if (len(psentence) < 50):
-                partials = [psentence]
-            else:
-                partials = splitsentence(psentence)
+            # Each sentence becomes a SRT transcript line with timing markers
+            for psentence in psentences:
 
-            for partial in partials:
-                wordcount = len(partial.split(' '))
-                start = ptimestamps[captionindex]['s']
-                end = ptimestamps[captionindex+(wordcount-1)]['e']
+                # Some sentences are too long; split up longer sentences into
+                # smaller chunks.
+                if (len(psentence) < 50):
+                    partials = [psentence]
+                else:
+                    partials = splitsentence(psentence)
 
-                starttimefmt = str(timedelta(seconds=start))[0:-3].replace('.', ',')
-                endtimefmt = str(timedelta(seconds=end))[0:-3].replace('.', ',')
+                for partial in partials:
+                    wordcount = len(partial.split(' '))
+                    start = ptimestamps[captionindex]['s']
+                    end = ptimestamps[captionindex+(wordcount-1)]['e']
 
-                print(linecount)
-                print(f'{starttimefmt} --> {endtimefmt}')
-                print(f'{partial}\n')
+                    if (lastendtime > timedelta(seconds=start)):
+                        # The transcript has a bad timing marker, indicating
+                        # that the previous partial ended after the start of
+                        # the next. Use the previous end marker as the start
+                        # instead.
+                        starttimefmt = str(lastendtime)[0:-3].replace('.', ',')
+                    else:
+                        starttimefmt = str(timedelta(seconds=start))[0:-3].replace('.', ',')
 
-                captionindex += wordcount
-                linecount += 1
+                    endtimefmt = str(timedelta(seconds=end))[0:-3].replace('.', ',')
+
+                    # Record the end time for this partial for comparison to
+                    # the next start time.
+                    lastendtime = timedelta(seconds=end)
+
+                    print(linecount)
+                    print(f'{starttimefmt} --> {endtimefmt}')
+                    print(f'{partial}\n')
+
+                    captionindex += wordcount
+                    linecount += 1
